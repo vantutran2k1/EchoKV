338e255b73bef1ac296262f8ebf70ad430aadcab
vantutran2k1 <vantu.tran2k1@gmail.com>
2025-10-11 17:09:16 +0700

    Bootstrap cluster on starting node

--- internal/raftnode/node.go
+++ internal/raftnode/node.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/hashicorp/raft"
 	raftboltdb "github.com/hashicorp/raft-boltdb/v2"
@@ -43,12 +44,19 @@ func (n *Node) Start() error {
 		return err
 	}
 
+	if err := n.setupRaftCore(); err != nil {
+		return err
+	}
+
 	serverListener, err := net.Listen("tcp", n.config.ListenAddr)
 	if err != nil {
 		return err
 	}
 	n.server = &serverListener
-	log.Printf("Node %s: Client server listening on %s", n.config.NodeID, n.config.ListenAddr)
+
+	go n.handleClientConnections()
+
+	log.Printf("node %s: client server listening on %s", n.config.NodeID, n.config.ListenAddr)
 
 	return nil
 }
@@ -87,4 +95,92 @@ func (n *Node) setupRaftStorage() error {
 	log.Printf("node %s: raft storage initialized in %s", n.config.NodeID, boltDBPath)
 
 	return nil
+}
+
+func (n *Node) setupRaftCore() error {
+	addr, err := net.ResolveTCPAddr("tcp", n.config.RaftAddr)
+	if err != nil {
+		return fmt.Errorf("failed to resolve raft addr: %w", err)
+	}
+
+	transport, err := raft.NewTCPTransport(
+		n.config.RaftAddr,
+		addr,
+		3,
+		10*time.Second,
+		os.Stdout,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create raft tcp transport: %w", err)
+	}
+
+	raftCfg := raft.DefaultConfig()
+	raftCfg.LocalID = raft.ServerID(n.config.NodeID)
+
+	ra, err := raft.NewRaft(
+		raftCfg,
+		&fsm{Store: n.store},
+		n.logStore,
+		n.stableStore,
+		n.snapshotStore,
+		transport,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create new raft node: %w", err)
+	}
+	n.raft = ra
+
+	lastIndex, err := n.logStore.LastIndex()
+	if err != nil {
+		return fmt.Errorf("failed to read last index from lgo store: %w", err)
+	}
+
+	if lastIndex == 0 {
+		if n.config.JoinAddr == "" {
+			log.Printf("node %s: bootstrapping new cluster...", n.config.NodeID)
+
+			configuration := raft.Configuration{
+				Servers: []raft.Server{
+					{
+						ID:      raftCfg.LocalID,
+						Address: transport.LocalAddr(),
+					},
+				},
+			}
+
+			future := n.raft.BootstrapCluster(configuration)
+			if err := future.Error(); err != nil {
+				if err != raft.ErrCantBootstrap {
+					return fmt.Errorf("failed to bootstrap cluster: %w", err)
+				}
+				log.Printf("node %s: cluster already bootstrapped by another node", n.config.NodeID)
+			}
+		} else {
+			log.Printf("node %s: started in join mode", n.config.NodeID)
+		}
+	} else {
+		log.Panicf("node %s: resuming raft operations from existing state (last index: %d)", n.config.NodeID, lastIndex)
+	}
+
+	return nil
+}
+
+func (n *Node) handleClientConnections() {
+	for {
+		conn, err := (*n.server).Accept()
+		if err != nil {
+			if opErr, ok := err.(*net.OpError); ok && opErr.Temporary() {
+				log.Printf("node %s: temporary error accepting connection: %v", n.config.NodeID, err)
+				continue
+			}
+			log.Printf("node %s: listener closed or permanent error: %v", n.config.NodeID, err)
+			return
+		}
+
+		go n.handleConnection(conn)
+	}
+}
+
+func (n *Node) handleConnection(conn net.Conn) {
+	conn.Close()
 }
